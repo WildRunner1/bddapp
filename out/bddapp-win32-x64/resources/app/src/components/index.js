aa9826d6bfc42c3e308ab7b5e13e433420a45744
WildRunner1 <80202217+WildRunner1@users.noreply.github.com>
2023-03-06 21:56:24 +0100

    jj

--- out/bddapp-win32-x64/resources/app/src/components/index.js
+++
@@ -1,9 +0,0 @@
-export {default as TruthTable} from'./truthtable';
-export {default as Teory} from'./teory';
-export {default as Robdd} from'./robdd';
-export {default as Home} from'./home';
-export {default as Header} from'./header';
-export {default as Footer} from'./footer';
-export {default as Diagrams} from'./diagrams';
-export {default as Bdd} from'./bdd';
-export {default as About} from'./about';
